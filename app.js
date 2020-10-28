9db6b6d1026ba325db6769765c7c26f99e569667
João Roberto <joao.roberto.pereira@accenture.com>
2020-10-28 10:56:55 -0300

    Correção no app.js

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
+const port = process.env.port || 3000;
 
 app.get('/', (req, res) => {
     res.send("Welcome to the home page");
 });
 
-app.listen(3000, () => {
-    console.log("Servidor Rodando na porta 3000");
+app.listen(port, () => {
+    console.log("Servidor Rodando na porta : "+ port);
 });
